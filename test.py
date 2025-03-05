937bacffa3096de373735e679579841c99843430
veluna <u220009@st.pu-toyama.ac.jp>
2025-03-06 01:36:29 +0900

    source1

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
-points = torch.zeros(6)
-points[0] = 4.0
-points[1] = 1.0
-points[2] = 3.0
-points[3] = 5.0
-points[4] = 2.0
-points[5] = 1.0
+import torch
+points = torch.tensor([4.0,1.0,5.0,3.0,2.0,1.0])
+
+print(points)
+
+print(float(points[0]), float(points[1]))#点の座標取得
+
+points = torch.tensor([[4.0,1.0],[5.0,3.0],[2.0,1.0]])#2次元の座標取得
+print(points)
+
+print(points.shape)
+
+print(points[0,1])
+print(points[0])
+
+points = torch.zeros(3,2)
+print(points)
